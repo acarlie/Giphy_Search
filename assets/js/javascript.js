e678921a9a770a895feaad63277491af3c5d4ab7
acarlie <ameliacarlie@gmail.com>
2019-08-02 14:43:15 -0400

    Added search object and methods, added api calls

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -9,20 +9,50 @@ var search = {
             search.button(arr[i]);
         });
     },
+    renderGif(obj){
+        var card = $('<div>').addClass('card').prependTo('#results');
+        var img = $('<img>').addClass('img').attr('src', obj.images.fixed_height_still.url).attr('data-still', obj.images.fixed_height_still.url).attr('data-gif', obj.images.fixed_height.url).appendTo(card);
+    },
     buttonClick(){
-        var searchTerm = $(this).text();
-        console.log(searchTerm);
+        var searchTerm = $(this).text().replace(' ', '%20');
+        var queryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=MhZnLZX3S3AQ3uqSWeeBpsJ8NXZXl54N&q=' + searchTerm + '&limit=5&offset=0&rating=G&lang=en';
+
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function(response){
+            var results = response.data;
+            $.each(results, function(i){
+                search.renderGif(results[i]);
+                console.log(results[i]);
+            })
+        });
+
+    },
+    imgClick(){
+        var still = $(this).attr('data-still');
+        var gif = $(this).attr('data-gif');
+        var src = $(this).attr('src');
+
+        if (src === still){
+            $(this).attr('src', gif);
+        } else {
+            $(this).attr('src', still);
+        }
     },
     submit(){
         event.preventDefault();
         var val = $('#input').val().trim();
-        search.button(val);
-
-        $('#input').val('');
+        if (val !== ''){
+            search.button(val);
+            $('#input').val('');
+        }
     }
 }
 
 search.renderButtons(search.buttons);
 
 $(document).on('click', '.btn-search', search.buttonClick);
-$('#submit').on('click', search.submit)
+$(document).on('click', '.img', search.imgClick);
+
+$('#submit').on('click', search.submit);
